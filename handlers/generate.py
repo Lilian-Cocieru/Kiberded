26c5c06606cde14b9135ac58c06d0b8f685d41a7
Lilian-Cocieru <lilianvideo.lc@gmail.com>
2025-05-27 20:24:55 +0300

    Refactor AI model configuration and update API key usage

--- handlers/generate.py
+++ handlers/generate.py
@@ -1,15 +1,17 @@
 from openai import AsyncOpenAI
-from config import AI_TOKEN
+from config import AI_TOKEN_D, AI_TOKEN_Q, AI_TOKEN_G, AI_TOKEN_M
 
 client = AsyncOpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key=AI_TOKEN,
+  api_key=AI_TOKEN_Q,
 )
 
 async def ai_generate(text: str):
   completion = await client.chat.completions.create(
     # model = "google/gemini-2.5-flash-preview-05-20",
-    model = "deepseek/deepseek-chat-v3-0324:free",
+    model="qwen/qwen3-30b-a3b:free",
+    # model="mistralai/devstral-small:free",
+    # model = "deepseek/deepseek-chat-v3-0324:free",
     messages=[
       {
         "role": "user",
