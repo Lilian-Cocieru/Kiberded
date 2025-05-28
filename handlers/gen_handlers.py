4103d7ff7b014447d50980a180569537c9752533
Lilian-Cocieru <lilianvideo.lc@gmail.com>
2025-05-28 06:26:43 +0300

    Refactor (handlers): functions and routing restructuring
    - deleted Hn.py and replaced with separate Gen_handlers.py and user_handlers.py
    - Updated Run.py to enable new routers: Gen_router and User_router

--- handlers/gen_handlers.py
+++ handlers/gen_handlers.py
@@ -7,25 +7,22 @@ from aiogram.fsm.context import FSMContext
 from handlers.generate import ai_generate
 
 
-router = Router()
+gen_router = Router()
 
 
 class Gen(StatesGroup):
-    wait = State
+    waiting = State
 
 
-@router.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer('Добро пожаловать! Напишите свой запрос.')
     
-@router.message(F.state == Gen.wait)
+@gen_router.message(F.state == Gen.waiting)
 async def stop_flood(message: Message):
     await message.answer('Подождите, ваш запрос генерируется')
     
     
-@router.message()
+@gen_router.message()
 async def generating(message: Message, state: FSMContext):
-    await state.set_state(Gen.wait)
+    await state.set_state(Gen.waiting)
     await ai_generate(message.text)
     response = await ai_generate(message.text)
     await message.answer(response) # можно было и так написать (response, parse_mode='Markdown')
--- run.py
+++ run.py
@@ -5,13 +5,14 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import TG_TOKEN
 
-from handlers.hn import router
-
+from handlers.gen_handlers import gen_router
+from handlers.user_handlers import user_router
 
 async def main():
     bot = Bot(token=TG_TOKEN)
     dp = Dispatcher()
-    dp.include_router(router)
+    dp.include_router(gen_router)
+    dp.include_router(user_router)
     await dp.start_polling(bot)
     
     
